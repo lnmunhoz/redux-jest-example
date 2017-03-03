f3dd4f5327597159cb7bbb163f61cd05348a2722
lnmunhoz <ln.munhoz@gmail.com>
2017-03-03 17:45:09 +0700

    Create test for addTodo

--- src/__tests__/reducers.js
+++ src/__tests__/reducers.js
@@ -1,10 +1,17 @@
 import expect from 'expect';
-import { ADD_TODO } from '../constants/actionTypes';
+import { addTodo } from '../actions/todos';
 import todosReducer from '../reducers/todos';
 
 describe('reducers', () => {
   it('should return the initial state', () => {
     const state = todosReducer(undefined, {});
     expect(state).toEqual([]);
+  })
+
+  it('should add a new todo to state', () => {
+    const state = todosReducer(undefined, addTodo('New todo'));
+    expect(state.length).toEqual(1);
+    expect(state[0].text).toEqual('New todo');
+    expect(state[0].done).toEqual(false);
   })
 })
